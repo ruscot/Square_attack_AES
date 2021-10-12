b57f9950e245a921a1fb8a7cea5a56b5594550d9
anthony <thonymartiinez@gmail.com>
2021-10-12 21:29:55 +0200

    reverse function for subbytes and shiftrow

--- cry_eng2021_tp_aessq/squareAttackAES.c
+++ cry_eng2021_tp_aessq/squareAttackAES.c
@@ -5,9 +5,9 @@
 #include "distinguisher.c"
 
 /**
- * For a given block on a given key compute the following operation ARK + SR +SB + ARK
+ * For a given block on a given key compute the following operation ARK + SR + SB + ARK
 */
-void aesDemiTurnBackward(uint8_t block[AES_BLOCK_SIZE], uint8_t round_key[AES_BLOCK_SIZE], int lastround) {
+void aesDemiTurnBackward(uint8_t block[AES_BLOCK_SIZE], uint8_t round_key[AES_BLOCK_SIZE]) {
     int i;
 	uint8_t tmp;
 
@@ -20,35 +20,38 @@ void aesDemiTurnBackward(uint8_t block[AES_BLOCK_SIZE], uint8_t round_key[AES_BL
 	}
 
     uint8_t* prev_key = malloc(sizeof(uint8_t) * 16);
-    prev_aes128_round_key(round_key, prev_key, 3);
-
+    prev_aes128_round_key(round_key, prev_key, 0);
+	printf("\n");
+	for(i = 0; i < 16; i++) {
+		printf("%d ", prev_key[i]);
+	}
 	/*
 	 * SubBytes + ShiftRow
 	 */
 	/* Row 0 */
-	block[ 0] = S[block[ 0]];
-	block[ 4] = S[block[ 4]];
-	block[ 8] = S[block[ 8]];
-	block[12] = S[block[12]];
+	block[ 0] = Sinv[block[ 0]];
+	block[ 4] = Sinv[block[ 4]];
+	block[ 8] = Sinv[block[ 8]];
+	block[12] = Sinv[block[12]];
 	/* Row 1 */
-	tmp = block[1];
-	block[ 1] = S[block[ 5]];
-	block[ 5] = S[block[ 9]];
-	block[ 9] = S[block[13]];
-	block[13] = S[tmp];
+	tmp = block[9];
+    block[ 9] = Sinv[block[ 5]];
+    block[ 5] = Sinv[block[ 1]];
+    block[ 1] = Sinv[block[13]];
+    block[13] = Sinv[tmp];
 	/* Row 2 */
 	tmp = block[2];
-	block[ 2] = S[block[10]];
-	block[10] = S[tmp];
+	block[ 2] = Sinv[block[10]];
+	block[10] = Sinv[tmp];
 	tmp = block[6];
-	block[ 6] = S[block[14]];
-	block[14] = S[tmp];
+	block[ 6] = Sinv[block[14]];
+	block[14] = Sinv[tmp];
 	/* Row 3 */
 	tmp = block[15];
-	block[15] = S[block[11]];
-	block[11] = S[block[ 7]];
-	block[ 7] = S[block[ 3]];
-	block[ 3] = S[tmp];
+    block[15] = Sinv[block[ 3]];
+    block[ 3] = Sinv[block[ 7]];
+    block[ 7] = Sinv[block[11]];
+    block[11] = Sinv[tmp];
     
 	/*
 	 * AddRoundKey
@@ -64,29 +67,139 @@ void aesDemiTurnBackward(uint8_t block[AES_BLOCK_SIZE], uint8_t round_key[AES_BL
  * byte of the correct key 
 */
 void guessCorrectKeyByte(uint8_t key[AES_BLOCK_SIZE], uint8_t block[AES_BLOCK_SIZE*256], uint8_t keyByte) {
-    //
+	printf("Start of guess correct bytes\n");
+    //Init the key to the S
+	int i, j;
+	for(i = 0; i < 16; i++){
+		key[i] = 0;
+	}
+	key[0] = keyByte;
+	//For all block get the last value and compute the sum to test if it's equal to 0
+	uint8_t* sum_block = malloc(sizeof(uint8_t) * 16);
+	uint8_t* current_block = malloc(sizeof(uint8_t) * 16);
+	initBlockToZero(sum_block);
+	for(i = 0; i < 256; i++){
+		for(j = 0; j < 16; j++) {
+			current_block[j] = block[i*16 + j];
+		}
+
+		aesDemiTurnBackward(current_block, key);
+		(*sum_block) ^= (*current_block);
+	}/*
+	for(j = 0; j < 16; j++) {
+		printf("%d ", sum_block[j]);
+	}
+	printf("\n");*/
+	for(j = 0; j < 16; j++) {
+		if(sum_block[j] == 0){
+			printf("\nKeyBytes %d for the bytes %d", keyByte, j);
+		}
+	}
+	free(sum_block);
+	free(current_block);
 }
 
 int main(){
-    printf("\nThis program implement the square attack on 3 rounds AES encryption\n");
+    printf("\nThis program implement the square attack on 3 rounds and 1/2 AES encryption\n");
 
     uint8_t *allCipheredMessage = malloc(sizeof(uint8_t) * 16 * 256);
+	uint8_t *prev_key = malloc(sizeof(uint8_t) * 16);
+	//uint8_t *other_key = malloc(sizeof(uint8_t) * 16);
+	//uint8_t *third_key = malloc(sizeof(uint8_t) * 16);
+    prev_key[ 0] = 0x85;
+    prev_key[ 1] = 0x7e;
+    prev_key[ 2] = 0x15;
+    prev_key[ 3] = 0x16;
+    prev_key[ 4] = 0x28;
+    prev_key[ 5] = 0xae;
+    prev_key[ 6] = 0xd2;
+    prev_key[ 7] = 0xa6;
+    prev_key[ 8] = 0x01;
+    prev_key[ 9] = 0x85;
+    prev_key[10] = 0x15;
+    prev_key[11] = 0x88;
+    prev_key[12] = 0x09;
+    prev_key[13] = 0xcf;
+    prev_key[14] = 0x4f;
+    prev_key[15] = 0x3c;
+	uint8_t *block = malloc(sizeof(uint8_t) * 16);
+	block[ 0] = 0x2b;
+    block[ 1] = 0x8e;
+    block[ 2] = 0x15;
+    block[ 3] = 0x26;
+    block[ 4] = 0x88;
+    block[ 5] = 0x0e;
+    block[ 6] = 0xd2;
+    block[ 7] = 0xa6;
+    block[ 8] = 0xbb;
+    block[ 9] = 0x57;
+    block[10] = 0x15;
+    block[11] = 0x58;
+    block[12] = 0x09;
+    block[13] = 0x8f;
+    block[14] = 0x4f;
+    block[15] = 0x9c;
+	uint8_t *next_key = malloc(sizeof(uint8_t) * 16);
+	uint8_t *save_block = malloc(sizeof(uint8_t) * 16);
+	save_block[ 0] = 0x2b;
+    save_block[ 1] = 0x8e;
+    save_block[ 2] = 0x15;
+    save_block[ 3] = 0x26;
+    save_block[ 4] = 0x88;
+    save_block[ 5] = 0x0e;
+    save_block[ 6] = 0xd2;
+    save_block[ 7] = 0xa6;
+    save_block[ 8] = 0xbb;
+    save_block[ 9] = 0x57;
+    save_block[10] = 0x15;
+    save_block[11] = 0x58;
+    save_block[12] = 0x09;
+    save_block[13] = 0x8f;
+    save_block[14] = 0x4f;
+    save_block[15] = 0x9c;
     //Initialize allCipheredMessage to 0
-    int i;
-    for(i = 0; i < 256; i++){
+    int i, j;
+	printf("\n");
+	for(i = 0; i < 16; i++) {
+		printf("%d ", prev_key[i]);
+	}
+	aes128_enc(block, prev_key, 1, 0);
+	next_aes128_round_key(prev_key, next_key, 0);	
+	aesDemiTurnBackward(block, next_key);
+	printf("\n");
+	for(i = 0; i < 16; i++) {
+		printf("%d ", block[i]);
+	}
+	printf("\n");
+	for(i = 0; i < 16; i++) {
+		printf("%d ", save_block[i]);
+	}
+
+
+    /*for(i = 0; i < 256; i++){
         initBlockToZero(allCipheredMessage + 16 * i);
-        allCipheredMessage[16*i] = i;
-        oracleSquareAttack(allCipheredMessage + 16 * i);
+		allCipheredMessage[16*i+5] = i;
+        //oracleSquareAttack(allCipheredMessage + 16 * i);
+		aes128_enc(allCipheredMessage + 16 * i, prev_key, 4, 0);
     }
-    
+    next_aes128_round_key(prev_key, third_key, 0);
+	next_aes128_round_key(third_key, other_key, 1);
+	next_aes128_round_key(other_key, third_key, 2);
+	printf("\nThird key :\n");
+	for(i = 0; i < 16; i++){
+        printf("%d ", third_key[i]);
+    }
+	printf("\n");
     //Init all possible byte for the key k 
     uint8_t *allPossibleKeyForEachByte = malloc(sizeof(uint8_t) * 16 * 256);
     for(i = 0; i < 256; i++){
         initBlockToValue(allPossibleKeyForEachByte + 16 * i, i);
     }
-
-    distinguisherRightOrWrongKeyGuessed();
+	guessCorrectKeyByte(prev_key, allCipheredMessage, 5);
+	*/
+    //distinguisherRightOrWrongKeyGuessed();
 
     free(allCipheredMessage);
+	//free(third_key);
     return 0;
 }
--- cry_eng2021_tp_aessq/testReverseDemiTurn.c
+++ cry_eng2021_tp_aessq/testReverseDemiTurn.c
@@ -3,23 +3,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include "distinguisher.c"
-/**
- * For a given block on a given key compute the following operation ARK + SR +SB + ARK
-*/
-void aesDemiTurnBackward(uint8_t block[AES_BLOCK_SIZE], uint8_t round_key[AES_BLOCK_SIZE]) {
-    int i;
+
+void aes_round2(uint8_t block[AES_BLOCK_SIZE], uint8_t round_key[AES_BLOCK_SIZE], int lastround, uint8_t block_without_key[AES_BLOCK_SIZE])
+{
+	int i;
 	uint8_t tmp;
-
-    /*
-	 * AddRoundKey
-	 */
-	for (i = 0; i < 16; i++)
-	{
-		block[i] ^= round_key[i];
-	}
-
-    uint8_t* prev_key = malloc(sizeof(uint8_t) * 16);
-    prev_aes128_round_key(round_key, prev_key, 3);
 
 	/*
 	 * SubBytes + ShiftRow
@@ -48,20 +36,93 @@ void aesDemiTurnBackward(uint8_t block[AES_BLOCK_SIZE], uint8_t round_key[AES_BL
 	block[11] = S[block[ 7]];
 	block[ 7] = S[block[ 3]];
 	block[ 3] = S[tmp];
-    
+
 	/*
+	 * MixColumns
+	 */
+	for (i = lastround; i < 16; i += 4) /* lastround = 16 if it is the last round, 0 otherwise */
+	{
+		uint8_t *column = block + i;
+		uint8_t tmp2 = column[0];
+		tmp = column[0] ^ column[1] ^ column[2] ^ column[3];
+
+		column[0] ^= tmp ^ xtime(column[0] ^ column[1]);
+		column[1] ^= tmp ^ xtime(column[1] ^ column[2]);
+		column[2] ^= tmp ^ xtime(column[2] ^ column[3]);
+		column[3] ^= tmp ^ xtime(column[3] ^ tmp2);
+	}
+	
+	for (i = 0; i < 16; i++)
+	{
+		block_without_key[i] = block[i];
+	}
+
+	/*
+	 * AddRoundKey
+	 */
+	for (i = 0; i < 16; i++)
+	{
+		block[i] ^= round_key[i];
+	}
+}
+
+void applyKeyInverse(uint8_t block[AES_BLOCK_SIZE], uint8_t round_key[AES_BLOCK_SIZE], uint8_t prev_key[AES_BLOCK_SIZE]) {
+    int i;
+	//uint8_t tmp;
+    prev_aes128_round_key(round_key, prev_key, 0);
+    /*
 	 * AddRoundKey
 	 */
 	for (i = 0; i < 16; i++)
 	{
 		block[i] ^= prev_key[i];
 	}
+
+    
+}
+
+/**
+ * For a given block on a given key compute the following operation ARK + SR +SB + ARK
+*/
+void aesDemiTurnBackward(uint8_t block[AES_BLOCK_SIZE], uint8_t round_key[AES_BLOCK_SIZE]) {
+    //int i;
+	uint8_t tmp;
+
+	/*
+	 * SubBytes + ShiftRow
+	 */
+
+    block[ 0] = Sinv[block[ 0]];
+	block[ 4] = Sinv[block[ 4]];
+	block[ 8] = Sinv[block[ 8]];
+	block[12] = Sinv[block[12]];
+
+	tmp = block[9];
+    block[ 9] = Sinv[block[ 5]];
+    block[ 5] = Sinv[block[ 1]];
+    block[ 1] = Sinv[block[13]];
+    block[13] = Sinv[tmp];
+
+	tmp = block[2];
+	block[ 2] = Sinv[block[10]];
+	block[10] = Sinv[tmp];
+	tmp = block[6];
+	block[ 6] = Sinv[block[14]];
+	block[14] = Sinv[tmp];
+
+	tmp = block[15];
+    
+    block[15] = Sinv[block[ 3]];
+    block[ 3] = Sinv[block[ 7]];
+    block[ 7] = Sinv[block[11]];
+    block[11] = Sinv[tmp];
+	
 }
 
 int main(){
-    printf("\nThis program test the reverse of a demi turn\n");
+    printf("\nThis program test the reverse of a demi turn\n\n");
     uint8_t *prev_key = malloc(sizeof(uint8_t) * 16);
-    
+    int i;
     prev_key[0] = 0x2b;
     prev_key[1] = 0x7e;
     prev_key[2] = 0x15;
@@ -114,20 +175,38 @@ int main(){
     previous_block[13] = 0x8f;
     previous_block[14] = 0x4f;
     previous_block[15] = 0x9c;
-    aes_round(block, prev_key, 16);
+    uint8_t *block_without_key = malloc(sizeof(uint8_t) * 16);
+    aes_round2(block, prev_key, 16, block_without_key);
     next_aes128_round_key(prev_key, next_key, 0);
+    uint8_t *prev_key_test = malloc(sizeof(uint8_t) * 16);
+    applyKeyInverse(block, next_key, prev_key_test);
+    //Check if the retrieve key is the good one
+    for(i = 0; i < 16; i++){
+        if(prev_key_test[i] != prev_key[i]){
+            printf("Problem in previous key !!\n");
+            break;
+        }
+    }
+    //Check if the block decrypt by the key is the good one
+    for(i = 0; i < 16; i++){
+        if(block_without_key[i] != block[i]){
+            printf("Problem in block without key !!\n");
+            break;
+        }
+    }
     aesDemiTurnBackward(block, next_key);
-    //Initialize allCipheredMessage to 0
-    int i;
+    //Check if the block decrypt by the subbyte + shift row is the same
     for(i = 0; i < 16; i++){
         if(previous_block[i] != block[i]){
-            printf("Problem in code decryption !!");
+            printf("Problem in code decryption !!\n");
+            break;
         }
     }
 
     free(previous_block);
     free(block);
     free(next_key);
+    free(prev_key_test);
     free(prev_key);
     return 0;
 }
