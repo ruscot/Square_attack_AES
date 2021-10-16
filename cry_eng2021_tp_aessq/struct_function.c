75d4d0700df229b9958582fb86f52d6fa5cb670d
anthony <thonymartiinez@gmail.com>
2021-10-16 08:22:38 +0200

    add commentary

--- cry_eng2021_tp_aessq/struct_function.c
+++ cry_eng2021_tp_aessq/struct_function.c
@@ -1,6 +1,15 @@
 #include "struct.h"
 #include <stddef.h>
+/**
+ * Some functions to check add, remove, check if 
+ * we have the value of the correct bytes for the key 
+ * we are trying to found
+*/
 
+/**
+ * After founding each correct bytes of the key
+ * put each byte in the key key.
+*/
 void put_guess_byte_in_key(head_list *list_byte, uint8_t *key){
     int i;
     for(i = 0; i < 16; i++){
@@ -8,6 +17,10 @@ void put_guess_byte_in_key(head_list *list_byte, uint8_t *key){
     }
 }
 
+/**
+ * Print each potential byte value for a given node
+ * in the linked list
+*/
 void print_byte_found_node(byte_found_node *byte_found){
     while(byte_found != NULL) {
         printf("%d ", byte_found->byte_found);
@@ -17,6 +30,9 @@ void print_byte_found_node(byte_found_node *byte_found){
     printf("\n");
 }
 
+/**
+ * Print the all the potential bytes found for the key
+*/
 void print_head_list(head_list *list_byte){
     printf("\nPrint all the byte found for the key guessed\n");
     int i;
@@ -26,6 +42,12 @@ void print_head_list(head_list *list_byte){
     }
 }
 
+/**
+ * If we are not in the first turn of the guessed key (we are testing on another A-set)
+ * If we found a byte in this turn we don't have to addd it to our linked list. If he's 
+ * in the list we just have to remember we have found it. So we set the in_list value
+ * of the byte to one for this turn.
+*/
 void other_turn_set_in_list(uint8_t index_byte, uint8_t byte_value, head_list *list_byte) {
     byte_found_node *current_byte_found_node = list_byte->index_key_byte[index_byte];
     while(current_byte_found_node != NULL) {
@@ -37,6 +59,10 @@ void other_turn_set_in_list(uint8_t index_byte, uint8_t byte_value, head_list *l
     }
 }
 
+/**
+ * For a new turn, on a new A-set, we set all the in_list value 
+ * of each byte guessed to 0
+*/
 void init_new_turn_byte(head_list *list_byte) {
     int i;
     byte_found_node *current_byte_found_node;
@@ -49,6 +75,9 @@ void init_new_turn_byte(head_list *list_byte) {
     }
 }
 
+/**
+ * At the end of a turn we removed all the false positive bytes
+*/
 void remove_all_byte_not_in_turn(uint8_t index_byte, head_list *list_byte) {
     byte_found_node *current_byte_found_node, *to_freed_node;
     while(list_byte->index_key_byte[index_byte]->in_list != 1) {
@@ -68,6 +97,9 @@ void remove_all_byte_not_in_turn(uint8_t index_byte, head_list *list_byte) {
     }
 }
 
+/**
+ * Add a guessed byte in our list if it is not already in.
+*/
 void add_byte_if_not_in(uint8_t index_byte, uint8_t byte_value, head_list *list_byte){
     if(list_byte->index_key_byte[index_byte] == NULL) {
         byte_found_node *current_byte_found_node = malloc(sizeof(byte_found_node));
@@ -94,9 +126,9 @@ void add_byte_if_not_in(uint8_t index_byte, uint8_t byte_value, head_list *list_
     }    
 }
 
-
-
-
+/**
+ * Initialize our list of guessed bytes (the head of the linked list)
+*/
 void init_list_byte(head_list *list_byte){
     int i;
     for(i = 0; i < 16; i++){
@@ -105,6 +137,10 @@ void init_list_byte(head_list *list_byte){
     }
 }
 
+/**
+ * Go through the list of supposed bytes a given index of the key
+ * to know if we have found the correct byte
+*/
 void check_byte_found_right_value(head_list *list_byte, uint8_t byte_to_check){
     int counter = 1;
     if(list_byte->index_key_byte[byte_to_check] == NULL){
@@ -121,10 +157,17 @@ void check_byte_found_right_value(head_list *list_byte, uint8_t byte_to_check){
     }
 }
 
+/**
+ * For a given index of the key return 0 if we still have 
+ * false positive byte
+*/
 int check_one_byte_founc(head_list *list_byte, uint8_t byte_to_check){
     return list_byte->found_right_byte[byte_to_check];
 }
 
+/**
+ * Check if for all of the index of the key we have foudn the right byte.
+*/
 int check_all_byte_found(head_list *list_byte){
     int i;
     for(i = 0; i < 16; i++){
